7b38a6ebb804a47997eb90ff66c11a6047b6de45
Austinwrap <aytmout@gmail.com>
2025-03-09 07:14:21 -0400

    Fix Netlify build errors: Update Chakra UI imports and remove deprecated Next.js config

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,6 +1,7 @@
 'use client';
 
-import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+import { ChakraProvider } from '@chakra-ui/react';
+import { extendTheme } from '@chakra-ui/theme-utils';
 import { ReactNode } from 'react';
 
 // Define the theme
--- next.config.js
+++ next.config.js
@@ -10,7 +10,7 @@ const nextConfig = {
   output: 'export',
   // Disable server components since we're using static export
   experimental: {
-    appDir: true,
+    // appDir is now the default in Next.js 14+, so we don't need to specify it
   },
   eslint: {
     // Warning: This allows production builds to successfully complete even if
